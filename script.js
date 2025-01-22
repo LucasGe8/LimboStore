b2a687876dd0183327c28f18d7a756903e76ecc1
Lucas <lucasge676@gmail.com>
2025-01-21 21:17:52 -0300

    Form Created

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const items = document.querySelector(".item-container");
 const cart = document.querySelector(".cart");
+const addItem = document.querySelector(".addItem")
+
 
 cart.onclick = function(){
     cart.classList.toggle("active");
 }
+
